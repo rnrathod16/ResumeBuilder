fc833f885bbe021c812194ab75f91b27eb4bd4ed
chowks <chowks05@gmail.com>
2021-10-03 13:57:40 +0800

    enhance UI, add forgot pass page

---
+++ src/ForgotPassword.jsx
@@ -0,0 +1,134 @@
+import React,{useState} from 'react';
+import back from './img/1.png';
+import bg from './img/bg.svg';
+import { useHistory} from 'react-router-dom';
+import EnvelopeSVG from '../src/img/envelope.svg';
+import LockSVG from '../src/img/lock.svg';
+import ConfirmPassSVG from '../src/img/confirmPass.svg';
+import './Form.css';
+
+function ForgotPassword(props){
+
+    
+    const history=useHistory();
+    const [user,setUser]= useState({
+        email: '',
+        otp: '',
+        password: '',
+        confirmPass: '',
+    });
+    const [verified, setVerified] = useState(false);
+    const [emailSubmitted, setEmailSubmit] = useState(false);
+
+    const eventIn = (event) => {
+        const {name, value}= event.target;
+
+        setUser((preValue) => ({
+            ...preValue,
+            [name]: value,
+        }));
+    };
+
+    const submiteve = async (e, name) => {
+        e.preventDefault();
+
+        // TODO: call post api
+
+        switch (name) {
+            case 'email':
+                setEmailSubmit(true);
+                break;
+            case 'otp':
+                setVerified(true);
+                break;
+            case 'reset':
+                history.push('/signin');
+                break;
+            default:
+                break;
+        }
+    };
+
+    const renderVerifyEmail = () => (
+        <>
+            <h2 style={{ color: `#3f3d56` }}>Verify Email</h2>
+            <div className="input-div one">
+                <div className="i">
+                    <img src={EnvelopeSVG} alt="fullName" className="regisFormIcon" />
+                </div>
+                <div className="div" >
+                    <input type="text" className="input" name="email" placeholder="Email" onChange={eventIn} value={user.email} />
+                </div>
+            </div>
+            <input onClick={(e) => submiteve(e, 'email')} type="submit" className="bt" value="Verify" />
+        </>
+    );
+
+    const renderVerifyOTP = () => (
+        <>
+            <h2 style={{ color: `#3f3d56` }}>Verify OTP From Email</h2>
+            <div className="input-div one">
+                <div className="i">
+                    <img src={LockSVG} alt="otp" className="regisFormIcon" />
+                </div>
+                <div className="div" >
+                    <input type="password" className="input" name="otp" placeholder="One time password" onChange={eventIn} value={user.otp} />
+                </div>
+            </div>
+            <input onClick={(e) => submiteve(e, 'otp')} type="submit" className="bt" value="Verify" />
+        </>
+    );
+
+    const renderResetPass = () => (
+        <>
+            <h2 style={{ color: `#3f3d56` }}>Reset password</h2>
+            <div className="input-div one">
+                <div className="i">
+                    <img src={LockSVG} alt="password" className="regisFormIcon" />
+                </div>
+                <div className="div" >
+                    <input type="password" className="input" name="password" placeholder="Password" onChange={eventIn} value={user.password} />
+                </div>
+            </div>
+            <div className="input-div pass">
+                <div className="i">
+                    <img src={ConfirmPassSVG} alt="confirmPassword" className="regisFormIcon" />
+                </div>
+                <div className="div">
+                    <input type="password" className="input" name="confirmPass" placeholder="Confirm password" onChange={eventIn} value={user.confirmPass} />
+                </div>
+            </div>
+            <input onClick={(e) => submiteve(e, 'reset')} type="submit" className="bt" value="Verify" />
+        </>
+    );
+
+    return(
+        <>
+            <body style={{ backgroundImage: `url(${back})` }} >
+                <div className="m " style={{ height:`100vh`}} >
+                    <div className="img" >
+                        <img src={bg} alt="bg" />
+                    </div>
+                    <div className="login-content" >
+                        <form method="POST" className="fo container-fluid col-md-9 shadow-lg p-3 mb-2 pt-2 bg-body rounded formContainer">
+                            <div className="forgotPassImage">
+                                <img src={LockSVG}  alt="Avatar"/>
+                            </div>
+                            {
+                                emailSubmitted
+                                    ? (
+                                        verified ? renderResetPass() : renderVerifyOTP() 
+                                    )
+                                    : renderVerifyEmail()
+                            }
+                        </form>
+                    </div>
+
+                </ div>
+            </body>
+        </>
+    );
+}
+
+export default ForgotPassword;
+
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,10 @@
 import React,{useState} from 'react';
-import back from './img/1.png'
-import bg from './img/bg.svg'
-import avtar from './img/avtar.svg'
+import back from './img/1.png';
+import bg from './img/bg.svg';
+import avtar from './img/avtar.svg';
 import { NavLink, useHistory} from 'react-router-dom';
+import UserSVG from '../src/img/user.svg';
+import LockSVG from '../src/img/lock.svg';
 import './Form.css';
 
 function Form(props){
@@ -81,26 +83,29 @@ function Form(props){
                             <img src={bg} alt="bg" />
                         </div>
                         <div className="login-content" >
-                            <form method="POST" className="fo container-fluid col-md-9 shadow-lg p-3 mb-2 pt-2 bg-body rounded">
-                                <img src={avtar}  alt="Avatar" style={{marginTop:`45px`}}/>
-                                <h2 style={{color:`#031bf1`}}> Welcome </h2>
+                            <form method="POST" className="fo container-fluid col-md-9 shadow-lg p-3 mb-2 pt-2 bg-body rounded formContainer">
+                                <img src={avtar}  alt="Avatar"/>
+                                <h2 style={{color:`#3f3d56`}}> Welcome </h2>
                                 <div className="input-div one">
-                                    <div className="i" >
-                                        <i className="fas fa-user" > </i>
-                                    </div> <div className="div" >
+                                <div className="i">
+                                    <img src={UserSVG} alt="fullName" className="regisFormIcon" />
+                                </div>
+                                    <div className="div" >
 
                                         <input type="text" className="input" name="username" placeholder="Username" onChange={eventIn} />
                                     </div>
                                 </div>
                                 <div className="input-div pass" >
-                                    <div className="i" >
-                                        <i className="fas fa-lock" > </i>
-                                    </div> <div className="div" >
-
+                                <div className="i">
+                                    <img src={LockSVG} alt="password" className="regisFormIcon" />
+                                </div>
+                                    <div className="div" >
                                         <input type="password" className="input mb-4" name="password" placeholder="Password" onChange={eventIn}/>
                                     </div>
                                 </div>
-                                <a href="/" className="mt-3" style={{textDecoration:`none`}}> Forgot Password ? </a>
+                                <div className="mt-3 fg-pass">
+                                    <NavLink to="/forgot_password" className="cnt dw" style={{textDecoration:`none`}}>Forgot password ?</NavLink>
+                                </div>
                                 <input onClick={submiteve} type="submit" className="bt" value="Login" />
                                <NavLink to="/" className="cnt" style={{textDecoration:`none`, marginBottom:`40px`}}>SIGNUP</NavLink>
                                
