2b4e8efb69162503f7555ba9c166f54f606468c7
rnrathod16 <rnrathod16@gmail.com>
2021-10-04 10:43:37 +0530

    Back Button css Modified OCT4_10:43

--- src/Back.jsx
+++ src/Back.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavLink} from 'react-router-dom';
-import "./Back.css"
+// import "./Back.css"
+import './Form.css';
+
 
 function Logout() {
 
     return (
 <>
 
-<NavLink to="/"><button className = "button">Back</button></NavLink>
+{/* <NavLink to="/"><button className = "bt">Back</button></NavLink> */}
+<NavLink to="/" className="cnt" style={{textDecoration:`none`, marginBottom:`40px`}}>Back</NavLink>
 </>
 
     );
